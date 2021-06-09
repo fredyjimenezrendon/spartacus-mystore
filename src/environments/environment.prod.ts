d3805ba4e73a553cd0d94405d814b8660695e337
Fredy Jimenez <fredy.alberto.jimenez@pivotree.com>
2021-06-09 09:07:38 -0500

    Trying to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   weatherAPI: {
-    url: 'http://api.openweathermap.org/data/2.5',
+    url: 'https://api.openweathermap.org/data/2.5',
     apiKey: 'fdb184bc246a9baced7d66c15b2d2a94',
     unit: 'metric'
   }
